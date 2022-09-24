808d85e1c8da32a11460e0c05745e9aad9727c49
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-09-24 22:46:01 +0600

    font family added and favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h2 className='text-center'>This is open auto</h2>
+      <h2 className='text-center text-3xl'>This is open auto</h2>
     </div>
   );
 };
