a1c81ddcf27a0ae8b8cf4caed59cbf52e5043277
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-09-25 11:57:54 +0600

    banner section responsive done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -15,21 +15,21 @@ const Banner = () => {
 
   return (
     <div className="container mx-auto px-6 ">
-      <div className="flex justify-between items-center gap-x-12 pt-16">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-x-12 pt-8 lg:pt-16 lg:pb-5">
         <div className='w-full lg:w-5/12 '>
-          <div className="text-4xl space-y-2  font-semibold">
+          <div className="text-2xl lg:text-4xl space-y-2 text-center lg:text-left font-semibold">
             <h2>Vehicle maintenance</h2>
             <h2>From The Comfort of</h2>
             <h2>Your Home</h2>
           </div>
-          <p className='text-lg py-8'>Open Auto Soothes the hassle of maintaining your vehicle and helps you  deal with unexpected repairs worry-free</p>
+          <p className='text-lg py-8 text-center lg:text-left text-[#9c9999]'>Open Auto Soothes the hassle of maintaining your vehicle and helps you  deal with unexpected repairs worry-free</p>
           <div>
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-2">
               <div className=''>
                 <input
                   type="text"
                   placeholder='Enter Your Name'
-                  className='px-3 w-7/12 py-2 border border-[#a8a8a8] rounded-2xl bg-[#0F141D]'
+                  className='px-3 text-sm w-full lg:w-7/12 py-2 border border-[#a8a8a8] rounded-2xl bg-[#0F141D]'
                   {...register('name', {
                     required: {
                       value: true,
@@ -43,7 +43,7 @@ const Banner = () => {
                 <input
                   type="email"
                   placeholder='Enter Your Email'
-                  className='px-3 w-7/12 py-2 border border-[#a8a8a8] rounded-2xl bg-[#0F141D]'
+                  className='px-3 text-sm w-full lg:w-7/12 py-2 border border-[#a8a8a8] rounded-2xl bg-[#0F141D]'
                   {...register('email', {
                     required: {
                       value: true,
@@ -57,19 +57,19 @@ const Banner = () => {
                 />
                 <p className='text-[13px] text-red-500 pl-2 pt-1'>{errors.email?.message}</p>
               </div>
-              <button className='w-7/12 py-2 border border-[#a8a8a8] rounded-2xl bg-[#0F141D] hover:bg-[#5703a1] hover:border-[#5703a1]'>Submit</button>
+              <button className='w-full lg:w-7/12 py-2 border border-[#a8a8a8] rounded-2xl bg-[#0F141D] hover:bg-[#5703a1] hover:border-[#5703a1]'>Submit</button>
             </form>
           </div>
         </div>
-        <div className='lg:w-7/12'>
+        <div className='lg:w-7/12 py-10 lg:py-0'>
           <img src={Pickup_Illustration} alt="Pickup_Illustration" />
         </div>
       </div>
       <div className='flex items-center'>
-        <div className='w-6/12 flex justify-end text-5xl'>
+        <div className='w-6/12 hidden lg:flex justify-end text-4xl'>
           <BsFillCaretDownSquareFill className='rounded-full cursor-pointer' />
         </div>
-        <div className='w-6/12 flex justify-end gap-x-3 pr-5'>
+        <div className='w-full lg:w-6/12 flex justify-evenly lg:justify-end gap-x-3 pr-5'>
           <a href="http://www.facebook.com" target="_blank" rel="noopener noreferrer"><RiFacebookFill className=' text-2xl' /></a>
           <a href="http://www.twitter.com" target="_blank" rel="noopener noreferrer"><RiTwitterFill className=' text-2xl' /></a>
           <a href="http://www.youtube.com" target="_blank" rel="noopener noreferrer"><RiYoutubeFill className=' text-2xl' /></a>
