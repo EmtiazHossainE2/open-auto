205f9ec96e3fa1579b02bb7903a9e5c51430cae7
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-09-25 13:01:05 +0600

    feature section responsive done

--- src/Pages/Home/Feature/Feature.jsx
+++ src/Pages/Home/Feature/Feature.jsx
@@ -4,14 +4,14 @@ import pickuService from '../../../Assets/picku service.png'
 const Feature = () => {
   return (
     <div className='container mx-auto px-5 py-20 bg-[#141821]'>
-      <div className="flex items-center gap-x-5">
-        <div className='w-1/2 flex justify-center items-center'>
+      <div className="flex flex-col-reverse lg:flex-row items-center gap-x-5 gap-y-12">
+        <div className='lg:w-1/2 flex justify-center items-center'>
           <img className='w-[75%]' src={pickuService} alt="Picku_Service" />
         </div>
-        <div className='w-1/2 '>
+        <div className='lg:w-1/2 text-center lg:text-left'>
           <h2 className='text-4xl font-semibold '>Focused on <br className='hidden lg:block'/> Time Saving</h2>
-          <p className='py-6 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis maxime enim aliquam fugit, reprehenderit sint voluptate, eveniet tempore doloremque, iusto quae. Quisquam reiciendis facere est aspernatur voluptatibus a in nulla dignissimos harum optio illum numquam, dolore, similique omnis repudiandae. Voluptatem quam tenetur amet repellat. Architecto in consequuntur, fugiat perferendis deserunt officiis, ratione libero amet illo quia unde aliquam repellendus eos, minima officia maiores et natus doloremque!</p>
-          <a className='border-2 rounded-3xl px-5 py-2 ' href='https://play.google.com/store' target="_blank" rel="noopener noreferrer"> Download the mobile app </a>
+          <p className='py-6 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis maxime enim aliquam fugit, reprehenderit sint voluptate, eveniet tempore doloremque, iusto quae. Quisquam reiciendis facere est aspernatur voluptatibus a in nulla dignissimos harum optio illum numquam, dolore, similique omnis repudiandae. Voluptatem quam tenetur amet repellat. Architecto in consequuntur, fugiat perferendis deserunt .</p>
+          <a className='border-2 rounded-3xl px-6 py-2 ' href='https://play.google.com/store' target="_blank" rel="noopener noreferrer"> Download the mobile app </a>
         </div>
       </div>
     </div>
